f1bb4f7943640a67750e542adfd9dedd8527e129
SamzFerg <SamzFerg@gmail.com>
2021-04-12 15:11:47 -0500

    Added methods for adding new assignments either one at a time or multiple at once. Additionally added methods to calculate average weighted and unweighted grades for a course

--- src/main/data/models/Course.java
+++ src/main/data/models/Course.java
@@ -9,6 +9,7 @@ package main.data.models;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -36,4 +37,48 @@ public class Course {
         this.gradeWeights = new HashMap<>();
         this.assignments = new HashMap<>();
     }
+
+    public void addAssignment(Assignment assignment) {
+        if(!assignments.containsKey(assignment.getName())) {
+            assignments.put(assignment.getName(), new LinkedList<>());
+        }
+        assignments.get(assignment.getName()).add(assignment);
+        calculateUnweightedGrade();
+        calculateWeightedGrade();
+    }
+
+    public void addMultipleAssignments(List<Assignment> newAssignments) {
+        newAssignments.stream().forEach((Assignment a) -> {
+            if(!assignments.containsKey(a.getName())) {
+                assignments.put(a.getName(), new LinkedList<>());
+            }
+            assignments.get(a.getName()).add(a);
+        });
+        calculateUnweightedGrade();
+        calculateWeightedGrade();
+    }
+
+    private void calculateUnweightedGrade() {
+       unweightedGrade = assignments.values()
+               .stream()
+               .flatMapToDouble( l -> l.stream().mapToDouble(Assignment::calculateGrade))
+               .average()
+               .getAsDouble();
+    }
+
+    private void calculateWeightedGrade() {
+        weightedGrade = 0;
+        assignmentTypes.stream()
+                .mapToDouble((key) -> gradeWeights.get(key).doubleValue() * assignments.get(key)
+                                                                        .stream()
+                                                                        .mapToDouble(Assignment::calculateGrade)
+                                                                        .average()
+                                                                        .getAsDouble())
+                .sum();
+    }
+
+    public double getUnweightedGrade() {
+        return unweightedGrade;
+    }
+
 }
